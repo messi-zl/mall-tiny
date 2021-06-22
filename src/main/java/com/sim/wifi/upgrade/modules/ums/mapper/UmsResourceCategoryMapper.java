67ed88723031f659893148e88d03aeabfa9f1542
li.zheng871 <li.zheng871@sim.com>
2021-06-22 13:05:51 +0800

    修改了目录名

---
+++ src/main/java/com/sim/wifi/upgrade/modules/ums/mapper/UmsResourceCategoryMapper.java
@@ -0,0 +1,16 @@
+package com.sim.wifi.upgrade.modules.ums.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sim.wifi.upgrade.modules.ums.model.UmsResourceCategory;
+
+/**
+ * <p>
+ * 资源分类表 Mapper 接口
+ * </p>
+ *
+ * @author macro
+ * @since 2020-08-21
+ */
+public interface UmsResourceCategoryMapper extends BaseMapper<UmsResourceCategory> {
+
+}
