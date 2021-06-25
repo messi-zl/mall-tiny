ad32843df97032c1f423e034ac598cfece903e47
li.zheng871 <li.zheng871@sim.com>
2021-06-25 19:10:45 +0800

    注释加上

--- src/main/java/com/macro/mall/tiny/config/MallSecurityConfig.java
+++ src/main/java/com/macro/mall/tiny/config/MallSecurityConfig.java
@@ -38,7 +38,7 @@ public class MallSecurityConfig extends SecurityConfig {
         return username -> adminService.loadUserByUsername(username); //userDetailsService(): SpringSecurity定义的核心接口，用于根据用户名获取用户信息，需要自行实现
     }
 
-    //动态权限
+    //动态权限 有则用，没有则不用
     @Bean
     public DynamicSecurityService dynamicSecurityService() {
         return new DynamicSecurityService() {
@@ -49,7 +49,7 @@ public class MallSecurityConfig extends SecurityConfig {
                 for (UmsResource resource : resourceList) {
                     map.put(resource.getUrl(), new org.springframework.security.access.SecurityConfig(resource.getId() + ":" + resource.getName()));
                 }
-                return map; //返回(资源url)
+                return map; //返回(资源url集)
             }
         };
     }
